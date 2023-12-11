b971378451fa45c429706bb2467281a828fd58a7
EC2 Default User <139383807+miho-baba@users.noreply.github.com>
2023-12-11 05:32:36 +0000

    更新機能の記述

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
   get '/top' => 'homes#top'
   post 'lists' => 'lists#create' # 投稿機能のためのルーティング
+  patch 'lists/:id' => 'lists#update', as: 'update_list'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
