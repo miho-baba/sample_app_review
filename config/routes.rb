8efac316fb639e71f41604fb34fe8c31023a3276
EC2 Default User <139383807+miho-baba@users.noreply.github.com>
2023-12-11 04:50:11 +0000

    編集機能の作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'lists/new'
   get 'lists' => 'lists#index'
   get 'lists/:id' => 'lists#show', as: 'list'
-  get 'lists/edit'
+  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
   get '/top' => 'homes#top'
   post 'lists' => 'lists#create' # 投稿機能のためのルーティング
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
